a185018dc72f9b1796ed6189ec1f844f78956d4e
Haliunaa <km632@duke.edu>
2023-12-01 18:41:47 -0500

    adding sample+ chatGPT code

---
+++ main.py
@@ -0,0 +1,77 @@
+import socket
+import json
+import threading
+
+class ChordNode:
+    def __init__(self, node_id, m=4):
+        self.node_id = node_id
+        self.m = m
+        self.finger_table = [None] * m
+        self.predecessor = None
+
+    def find_predecessor(self, key):
+        # Simplified find_predecessor method
+        pass
+
+    def find_successor(self, key):
+        # Simplified find_successor method
+        pass
+
+    def notify(self, new_node):
+        # Simplified notify method
+        pass
+
+    def join(self, existing_node):
+        # Join the Chord ring by updating finger table entries
+        predecessor = existing_node.find_predecessor(self.node_id)
+        self.finger_table[0] = existing_node.find_successor(self.node_id)
+
+        for i in range(1, self.m):
+            entry_id = (self.node_id + 2**i) % (2**self.m)
+            self.finger_table[i] = existing_node.find_successor(entry_id)
+
+        # Notify existing nodes about the new node
+        existing_node.notify(self)
+
+    def start_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('localhost', self.node_id))
+        server_socket.listen()
+
+        print(f"Node {self.node_id} listening on port {self.node_id}")
+
+        while True:
+            client_socket, addr = server_socket.accept()
+            threading.Thread(target=self.handle_connection, args=(client_socket,)).start()
+
+    def handle_connection(self, client_socket):
+        data = client_socket.recv(1024).decode('utf-8')
+        request = json.loads(data)
+
+        if request['type'] == 'find_successor':
+            key = request['key']
+            successor = self.find_successor(key)
+            response = {'successor': successor.node_id if successor else None}
+            client_socket.send(json.dumps(response).encode('utf-8'))
+
+        client_socket.close()
+
+# Function to create a new Chord node when a new node joins
+def create_chord_node(node_id, existing_node):
+    new_node = ChordNode(node_id)
+    threading.Thread(target=new_node.start_server).start()
+    new_node.join(existing_node)
+    return new_node
+
+# Example Usage
+if __name__ == "__main__":
+    node4 = ChordNode(4)
+    node8 = ChordNode(8)
+
+    threading.Thread(target=node4.start_server).start()
+    threading.Thread(target=node8.start_server).start()
+
+    new_node = create_chord_node(1, node8)
+
+    # Print the Chord ring
+    print_ring([node4, node8, new_node])
---
+++ sample_code.py
@@ -0,0 +1,145 @@
+from pprint import pprint
+
+def binarySearch(alist, item):
+    """
+    binary search given alist and item to search for
+    returns is element was found, otherwise return the last midpoint before search failed
+    last midpoint is always either the predessecor or succesor to item (if the item is not found)
+    """
+    first = 0
+    last = len(alist)-1
+    found = False
+    
+    while first<=last and not found:
+        midpoint = (first + last)//2
+        if alist[midpoint] == item:
+            found = True
+        else:
+            if item < alist[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
+    
+    return found, midpoint
+
+def successor(L, n):
+    """
+    Given a sorted list of ints L, and an integer n, return the smallest element of L that
+    is larger than or equal to n. If none exist, return None
+    """
+    found, midpoint = binarySearch(L,n)
+    if L[midpoint] >= n:
+        return L[midpoint]
+    if L[midpoint] < n:
+        if midpoint==len(L)-1:
+            return None
+        return L[midpoint+1]
+
+
+def get_fingers(nodes, m):
+    """
+    build finger table, given a list of nodes, and m
+    """
+    nodes_augment = nodes.copy()
+    nodes_augment = sorted(nodes_augment)
+    maxnum = pow(2,m)
+    # wraparound first node to accomodate nodes clockwise of n when n is larger than the largest valued node
+    nodes_augment.append(nodes_augment[0]+maxnum)
+    
+    finger_table = {}
+    for node in nodes: 
+        fingers = [None]*m
+        for i in range(m):
+            j = (node + pow(2,i))%maxnum
+            fingers[i] = successor(nodes_augment,j)%maxnum
+        finger_table[node] = fingers.copy()
+        
+    return finger_table
+
+def get_key_loc(nodes, k, m):
+    """
+    given a key location k, and ring of size 2^m, and list of nodes, give the location of the key in the DHT
+    """
+    nodes_augment = nodes.copy()
+    nodes_augment = sorted(nodes_augment)
+    maxnum = pow(2,m)
+    # wraparound first node to accomodate nodes clockwise of n when n is larger than the largest valued node
+    nodes_augment.append(nodes_augment[0]+maxnum)
+    return successor(nodes_augment, k)%maxnum
+
+
+def get_query_path(nodes, k, n, m):
+    """
+    compute the quey path, given a list of nodes in the DHT, the key k to be found, origin node n, and ring of size 2^m
+    """
+    nodes_copy = nodes.copy()
+    nodes_copy = sorted(nodes_copy)
+    
+    k_loc = get_key_loc(nodes_copy, k, m) # location of key k
+    fingers = get_fingers(nodes_copy, m) # finger table
+    query_path = [n] # start the path with origin node
+    
+    while(True): #keep going until the correct key location is found
+        
+        if k_loc==n: # return the path if correct location found
+            return query_path
+
+        f = fingers[n]
+        
+        i = 0
+        x = f[i]
+        
+        if k_loc > n: 
+            # if the clockwise path between the origin node (n) and the key location (k_loc) DOES NOT include 0, all the nodes that
+            # lie in that path (x) must satisfy k_loc> x >n
+            while (x < k_loc) and (x > n):
+                i += 1
+                if i == len(f):
+                    break
+                x = f[i]
+                
+        if k_loc < n: 
+            # if the clockwise path between the origin node and the key location includes 0, all the nodes that
+            # lie in that path (x) must satisfy (k_loc < x) && (n < x) OR (k_loc > x) && (n > x)
+            while ( ((x > k_loc) and (x > n)) or ((x < k_loc) and (x < n)) ):
+                i += 1
+                if i == len(f):
+                    break
+                x = f[i]
+        
+        # update next node to query, and append to path
+        if i==0:
+            n = f[0]
+        else:
+            n = f[i-1]
+            
+        query_path.append(n)
+    
+if __name__=="__main__":
+    
+    # Chord DHT as described in Lecture 5 of week 3 of the Coursera Cloud Computing Concepts Part 1 Course
+    
+    nodes = [112, 96, 80, 16, 32, 45] # nodes in the P2P system
+    m = 7 # Ring is of size 2^m
+    print('Chord DHT with peers {nodes}, and m = {m} \n'.format(nodes=nodes, m=7))
+    
+    print('The finger table for this DHT is: ')
+    pprint(get_fingers(nodes, m))
+    print()
+    
+    file_key = 42
+    print('File location for file key {file_key} is at node {file_key_location} \n'.format(
+        file_key=file_key, file_key_location=get_key_loc(nodes, file_key, m)
+    ))
+    query_node = 80
+    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
+        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
+    ))
+    query_node = 96
+    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
+        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
+    ))
+    query_node = 45
+    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
+        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
+    ))
